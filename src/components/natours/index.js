9c978637c7233f8b5267331c57d6c7b9c4b32084
Evelyn <ks0430@163.com>
2018-12-29 11:36:57 +1000

    Finish story & form section

--- src/components/natours/index.js
+++ src/components/natours/index.js
@@ -10,6 +10,9 @@ import nat3 from "../../img/nat-3-large.jpg";
 import nat8 from "../../img/nat-8.jpg";
 import nat9 from "../../img/nat-9.jpg";
 
+import video from "../../img/video.mp4";
+import videoWeb from "../../img/video.webm";
+
 export default class Natours extends Component {
   render() {
     return (
@@ -204,7 +207,141 @@ export default class Natours extends Component {
             </div>
           </section>
 
-          <section className="section-stories">Stories</section>
+          <section className="section-stories">
+            <div className="u-center-text u-margin-bottom-md">
+              <h2 className="heading-secondary">
+                We make people genuinely happy
+              </h2>
+            </div>
+            <div className="bg-video">
+              <video className="bg-video__content" autoPlay muted loop>
+                <source src={video} type="video/mp4" />
+                <source src={videoWeb} type="video/webm" />
+                Your browser is not supported!
+              </video>
+            </div>
+
+            <div className="row">
+              <div className="story">
+                <figure className="story__shape">
+                  <img
+                    src={nat9}
+                    alt="Person on a tour"
+                    className="story__img"
+                  />
+                  <figcaption className="story__caption">
+                    Marry Smith
+                  </figcaption>
+                </figure>
+                <div className="story__text">
+                  <h3 className="heading-tertiary u-margin-bottom-sm">
+                    I had the best week ever with my family
+                  </h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Illum repellat officiis architecto consectetur nisi maiores
+                    alias veritatis suscipit unde. Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Illum repellat officiis
+                    architecto consectetur nisi maiores alias veritatis suscipit
+                    unde.
+                  </p>
+                </div>
+              </div>
+              <div className="story">
+                <figure className="story__shape">
+                  <img
+                    src={nat8}
+                    alt="Person on a tour"
+                    className="story__img"
+                  />
+                  <figcaption className="story__caption">
+                    Marry Smith
+                  </figcaption>
+                </figure>
+                <div className="story__text">
+                  <h3 className="heading-tertiary u-margin-bottom-sm">
+                    I had the best week ever with my family
+                  </h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Illum repellat officiis architecto consectetur nisi maiores
+                    alias veritatis suscipit unde. Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Illum repellat officiis
+                    architecto consectetur nisi maiores alias veritatis suscipit
+                    unde.
+                  </p>
+                </div>
+              </div>
+              <div className="u-center-text u-margin-top-md">
+                <a href="#" className="btn-text">
+                  Read all stories
+                </a>
+              </div>
+            </div>
+          </section>
+
+          <section className="section-book">
+            <div className="book">
+              <div className="book__form">
+                <form action="#" className="form">
+                  <div className="u-margin-bottom-xm">
+                    <h2 className="heading-secondary">Start booking now</h2>
+                  </div>
+                  <div className="form__group">
+                    <input
+                      id="name"
+                      type="text"
+                      className="form__input"
+                      placeholder="Full Name"
+                      required
+                    />
+                    <label htmlFor="name" className="form__label">
+                      Full name
+                    </label>
+                  </div>
+                  <div className="form__group">
+                    <input
+                      id="email"
+                      type="email"
+                      className="form__input"
+                      placeholder="Email"
+                      required
+                    />
+                    <label htmlFor="email" className="form__label">
+                      Email
+                    </label>
+                  </div>
+                  <div className="form-group u-margin-bottom-xm">
+                    <div className="form__radio-group">
+                      <input
+                        type="radio"
+                        className="form__radio-input"
+                        id="small"
+                        name="size"
+                      />
+                      <label htmlFor="small" className="form__radio-label">
+                        <span className="form__radio-button" />
+                        Small tour group
+                      </label>
+                    </div>
+                    <div className="form__radio-group">
+                      <input
+                        type="radio"
+                        className="form__radio-input"
+                        id="large"
+                        name="size"
+                      />
+                      <label htmlFor="large" className="form__radio-label">
+                        <span className="form__radio-button" />
+                        Large tour group
+                      </label>
+                    </div>
+                  </div>
+                  <button className="btn btn--green">Next Step</button>
+                </form>
+              </div>
+            </div>
+          </section>
         </main>
 
         <div className="popup" id="popup">
