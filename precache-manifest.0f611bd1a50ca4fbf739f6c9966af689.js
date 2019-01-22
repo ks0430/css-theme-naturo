21a3a6f7aea2e2605f2c104b33dedb6667bf62f8
Evelyn <ks0430@163.com>
2019-01-22 16:31:09 +1000

    Updates

---
+++ precache-manifest.0f611bd1a50ca4fbf739f6c9966af689.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "f556a2c3f37c228f95a5eac3fae8c4a7",
+    "url": "/css-theme-naturo/static/media/video.f556a2c3.webm"
+  },
+  {
+    "revision": "c4b98b46a9b2deccd720",
+    "url": "/css-theme-naturo/static/css/main.9c2d7aeb.chunk.css"
+  },
+  {
+    "revision": "c5b0b0d22763ddca2c04",
+    "url": "/css-theme-naturo/static/js/1.c5b0b0d2.chunk.js"
+  },
+  {
+    "revision": "8f17ddd1d68c4631bdd5",
+    "url": "/css-theme-naturo/static/js/runtime~main.8f17ddd1.js"
+  },
+  {
+    "revision": "14efd82b81787a9764b2cb7eb7afd3e8",
+    "url": "/css-theme-naturo/static/media/nat-8.14efd82b.jpg"
+  },
+  {
+    "revision": "b525a99bcf20d08b2aaf6dd83b7be623",
+    "url": "/css-theme-naturo/static/media/nat-9.b525a99b.jpg"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/css-theme-naturo/static/media/logo-white.40d4ef82.png"
+  },
+  {
+    "revision": "7fdf2a886516e020e300ae6bd544ab7d",
+    "url": "/css-theme-naturo/static/media/nat-1-large.7fdf2a88.jpg"
+  },
+  {
+    "revision": "7a25f1999a15760508f5e6131245aeac",
+    "url": "/css-theme-naturo/static/media/nat-2-large.7a25f199.jpg"
+  },
+  {
+    "revision": "417d3d741a7a47b3d6ef0307228033a0",
+    "url": "/css-theme-naturo/static/media/nat-3-large.417d3d74.jpg"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/css-theme-naturo/static/media/video.ffcd876c.mp4"
+  },
+  {
+    "revision": "c4b98b46a9b2deccd720",
+    "url": "/css-theme-naturo/static/js/main.c4b98b46.chunk.js"
+  },
+  {
+    "revision": "61bbf12ae90a30cd432d77087921a02f",
+    "url": "/css-theme-naturo/static/media/nat-4.61bbf12a.jpg"
+  },
+  {
+    "revision": "5f846d16e6be9a73e6eb73f620379af2",
+    "url": "/css-theme-naturo/static/media/nat-7.5f846d16.jpg"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/css-theme-naturo/static/media/nat-10.2797561f.jpg"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/css-theme-naturo/static/media/hero.6e09f410.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/css-theme-naturo/static/media/nat-5.19f9a22f.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/css-theme-naturo/static/media/nat-6.1ef6d764.jpg"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/css-theme-naturo/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/css-theme-naturo/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/css-theme-naturo/static/media/linea-basic-10.e6763f00.ttf"
+  },
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/css-theme-naturo/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "3d6a22a0307c014029f4e851778e18e7",
+    "url": "/css-theme-naturo/index.html"
+  }
+];
